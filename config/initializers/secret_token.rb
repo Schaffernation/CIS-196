818c109763414f09493f3e5c76ce478df9de1b4a
Joseph Schaffer <schaffernation@gmail.com>
2012-09-05 21:29:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minusdollar::Application.config.secret_token = '8de2f42364e2e08268f589b20ba8f392e18bd3f439113e472b7d495a45d1d20bf15d7b072b8535e2586f5a6cdf86774574d805a96bf4454d613b48640fbf5147'
